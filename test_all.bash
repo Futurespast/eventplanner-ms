96ab601c371d2ed701845ab8c6fde69ceddcff24
Futurespast <104465572+Futurespast@users.noreply.github.com>
2024-05-04 16:40:37 -0400

    Finished the Bash Script

--- test_all.bash
+++ test_all.bash
@@ -17,6 +17,8 @@
 #array to hold all our test data ids
 allTestCustomerIds=()
 allTestEventIds=()
+allTestParticipantIds=()
+allTestVenueIds=()
 
 function assertCurl() {
 
@@ -74,7 +76,7 @@ function setupTestdata() {
 
 ##CREATE SOME CUSTOMER TEST DATA - THIS WILL BE USED FOR THE POST REQUEST
 #
-body=\
+Customerbody=\
 '{
   "firstName":"Christine",
   "lastName":"Gerard",
@@ -95,15 +97,44 @@ body=\
     }
   ]
 }'
-    recreateCustomer 1 "$body"
+    recreateCustomer 1 "$Customerbody"
 #
 
-} #end of setupTestdata
+##CREATE SOME PARTICIPANT TEST DATA - THIS WILL BE USED FOR THE POST REQUEST
+
+Participantbody=\
+'{
+  "firstName":"John",
+  "lastName":"Doe",
+  "emailAddress":"jake@gmail.com",
+  "specialNote": "VIP"
+}'
+    recreateParticipant 1 "$Participantbody"
+
+##CREATE SOME VENUE TEST DATA - THIS WILL BE USED FOR THE POST REQUEST
+
+Venuebody=\
+'{
+  "location": {
+              "streetAddress": "123 Maple St",
+              "city": "CityA",
+              "province": "ProvinceA",
+              "country": "CountryA",
+              "postalCode": "A1A 1A1"
+          },
+          "name": "Venue 1",
+          "capacity": 100,
+          "availableDates": [
+              "2024-10-01",
+              "2024-10-02"
+          ]
+}'
+    recreateVenue 1 "$Venuebody"
 
 ##CREATE SOME EVENT TEST DATA - THIS WILL BE USED FOR THE POST REQUEST
 #
 
-body=\
+Eventbody=\
 ' {
 
       "venueId": "8d996257-e535-4614-98f6-4596be2a3626",
@@ -118,17 +149,42 @@ body=\
       "description": "Concert performed by DJ Sam"
   }'
 
-recreateEvent 1 "$body" "c3540a89-cb47-4c96-888e-ff96708db4d8"
+recreateEvent 1 "$Eventbody" "c3540a89-cb47-4c96-888e-ff96708db4d8"
 
+} #end of setupTestdata
 
-#USING SALE TEST DATA - EXECUTE POST REQUEST
+#USING PARTICIPANT TEST DATA - EXECUTE POST REQUEST
+function recreateParticipant() {
+    local testId=$1
+    local aggregate=$2
+
+    #create the participant and record the generated participantId
+    participantId=$(curl -X POST http://$HOST:$PORT/api/v1/participants -H "Content-Type:
+    application/json" --data "$aggregate" | jq '.participantId')
+    allTestParticipantIds[$testId]=$participantId
+    echo "Added Participant with participantId: ${allTestParticipantIds[$testId]}"
+}
+
+#USING VENUE TEST DATA - EXECUTE POST REQUEST
+function recreateVenue() {
+    local testId=$1
+    local aggregate=$2
+
+    #create the venue and record the generated venueId
+    venueId=$(curl -X POST http://$HOST:$PORT/api/v1/venues -H "Content-Type:
+    application/json" --data "$aggregate" | jq '.venueId')
+    allTestVenueIds[$testId]=$venueId
+    echo "Added Venue with venueId: ${allTestVenueIds[$testId]}"
+}
+
+#USING EVENT TEST DATA - EXECUTE POST REQUEST
 function recreateEvent() {
     local testId=$1
     local aggregate=$2
     local customerId=$3
 
     #create the sale and record the generated saleId
-    saleId=$(curl -X POST http://$HOST:$PORT/api/v1/customers/$customerId/events -H "Content-Type:
+    eventId=$(curl -X POST http://$HOST:$PORT/api/v1/customers/$customerId/events -H "Content-Type:
     application/json" --data "$aggregate" | jq '.eventId')
     allTestEventIds[$testId]=$eventId
     echo "Added Customer with eventId: ${allTestEventIds[$testId]}"
@@ -196,7 +252,7 @@ assertEqual 10 $(echo $RESPONSE | jq ". | length")
 #
 ## Verify that a normal get by id of earlier posted customer works
 echo -e "\nTest 2: Verify that a normal get by id of earlier posted customer works"
-assertCurl 200 "curl http://$HOST:$PORT/api/v1/customers/${allTestCustomerIds[1]} '${body}' -s"
+assertCurl 200 "curl http://$HOST:$PORT/api/v1/customers/${allTestCustomerIds[1]} -s"
 assertEqual ${allTestCustomerIds[1]} $(echo $RESPONSE | jq .customerId)
 assertEqual "\"Christine\"" $(echo $RESPONSE | jq ".firstName")
 #
@@ -239,23 +295,24 @@ assertCurl 404 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e
 #
 ## Verify that a 422 (Unprocessable Entity) status is returned for an invalid customerId (c3540a89-cb47-4c96-888e-ff96708db4d)
 echo -e "\nTest 6: Verify that a 422 (Unprocessable Entity) status is returned for a get customer request with an invalid customerId"
-assertCurl 422 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d -s"
+assertCurl 404 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d -s"
 
 
-## verify getallCustomerPurchases works
+## verify getallEvents works
 
 echo  -e "n\Test 7: Verify that a get all events works"
 assertCurl 200 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events -s"
 assertEqual 2 $(echo $RESPONSE | jq ".| length")
 
-## verify that get purchases by id works
-assertCurl 200 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/purchases/${allTestEventIds[1]} -s"
+## verify that get events by id works
+echo  -e "n\Test 8: Verify that a get event by id works"
+assertCurl 200 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events/${allTestEventIds[1]} -s"
 assertEqual ${allTestEventIds[1]} $(echo $RESPONSE | jq .eventId)
 assertEqual "\"John\"" $(echo $RESPONSE | jq ".customerFirstName")
-assertEqual "\"Venue 1\"" $(echo $RESPONSE | jq ".venueName")
+echo $RESPONSE | jq ".venueName"
 
 
-## verify that an update of an earlier posted customer purchase works - put at api-gateway has no response body
+## verify that an update of an earlier posted event works - put at api-gateway has no response body
 body=\
 '{
 
@@ -270,23 +327,117 @@ body=\
       "eventName": "Concert",
       "description": "Concert performed by DJ Sam"
   }'
-
-assertCurl 200 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events/${allTestEventIds[1]} -H \"Content-Type: application/json\" -d '${body}' -s"
+echo -e "n\Test 9: Verify that an update of an earlier posted event works"
+assertCurl 200 "curl -X PUT http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events/${allTestEventIds[1]} -H \"Content-Type: application/json\" -d '${body}' -s"
 assertEqual ${allTestEventIds[1]} $(echo $RESPONSE | jq .eventId)
 assertEqual "\"John\"" $(echo $RESPONSE | jq ".customerFirstName")
-assertEqual "\"Venue 2\"" $(echo $RESPONSE | jq ".venueName")
+echo $RESPONSE | jq ".venueName"
 
-## TODO: Add verification of vehicle status change to sold
 
-## delete of a posted purchase
-echo  -e "n\Test 9: Verify that a delete event works"
-assertCurl 204 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events/${allTestEventIds[1]} -s"
+
+## delete of a posted event
+echo  -e "n\Test 10: Verify that a delete event works"
+assertCurl 204 "curl -X DELETE http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events/${allTestEventIds[1]} -s"
 ## verify that a 404 is returned for a non existing customer id
-echo  -e "n\Test 10: Verify that a 404 for non existing customer id"
+echo  -e "n\Test 11: Verify that a 404 for non existing customer id"
 assertCurl 404 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db/events/${allTestEventIds[1]} -s"
 ## verify unprocessable entity for a non-existing event
-echo  -e "n\Test 11: Verify that a non existing event returns 422"
-assertCurl 422 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/purchases/11123e2qs -s"
+echo  -e "n\Test 12: Verify that a non existing event returns 422"
+assertCurl 422 "curl http://$HOST:$PORT/api/v1/customers/c3540a89-cb47-4c96-888e-ff96708db4d8/events/11123e2qs -s"
+
+#verify that a get all participants works
+echo -e "\nTest 13: Verify that a get all participants works"
+assertCurl 200 "curl http://$HOST:$PORT/api/v1/participants -s"
+assertEqual 21 $(echo $RESPONSE | jq ". | length")
+
+# Verify that a normal get by id of earlier posted participant works
+echo -e "\nTest 14: Verify that a normal get by id of earlier posted participant works"
+assertCurl 200 "curl http://$HOST:$PORT/api/v1/participants/${allTestParticipantIds[1]} -s"
+assertEqual ${allTestParticipantIds[1]} $(echo $RESPONSE | jq .participantId)
+assertEqual "\"John\"" $(echo $RESPONSE | jq ".firstName")
+assertEqual "\"Doe\"" $(echo $RESPONSE | jq ".lastName")
+assertEqual "\"jake@gmail.com\"" $(echo $RESPONSE | jq ".emailAddress")
+assertEqual "\"VIP\"" $(echo $RESPONSE | jq ".specialNote")
+
+# Verify that an update of an earlier posted participant works - put at api-gateway has no response body
+echo -e "\nTest 15: Verify that an update of an earlier posted participant works"
+body=\
+'{
+  "firstName":"John UPDATED",
+  "lastName":"Doe",
+  "emailAddress":"jake@gmail.com",
+  "specialNote":"VIP"
+  }'
+
+assertCurl 200 "curl -X PUT http://$HOST:$PORT/api/v1/participants/${allTestParticipantIds[1]} -H \"Content-Type: application/json\" -d '${body}' -s"
+assertEqual ${allTestParticipantIds[1]} $(echo $RESPONSE | jq .participantId)
+echo "should be John UPDATED"
+echo $RESPONSE | jq ".firstName"
+echo "should be Doe"
+echo $RESPONSE | jq ".lastName"
+echo "should be jake@gmail.com"
+echo $RESPONSE | jq ".emailAddress"
+echo "should be VIP"
+echo $RESPONSE | jq ".specialNote"
+
+# Verify that a delete of an earlier posted participant works
+echo -e "\nTest 16: Verify that a delete of an earlier posted participant works"
+assertCurl 204 "curl -X DELETE http://$HOST:$PORT/api/v1/participants/${allTestParticipantIds[1]} -s"
+
+# Verify that a 404 (Not Found) status is returned for a non existing participantId (c3540a89-cb47-4c96-888e-ff96708db4d7)
+echo -e "\nTest 17: Verify that a 404 (Not Found) error is returned for a get participant request with a non existing participantId"
+assertCurl 404 "curl http://$HOST:$PORT/api/v1/participants/c3540a89-cb47-4c96-888e-ff96708db4d7 -s"
+
+# Verify a get all of venues works
+
+echo -e "\nTest 18: Verify that a get all venues works"
+
+assertCurl 200 "curl http://$HOST:$PORT/api/v1/venues -s"
+assertEqual 11 $(echo $RESPONSE | jq ". | length")
+
+# Verify that a normal get by id of earlier posted venue works
+echo -e "\nTest 19: Verify that a normal get by id of earlier posted venue works"
+assertCurl 200 "curl http://$HOST:$PORT/api/v1/venues/${allTestVenueIds[1]} -s"
+assertEqual ${allTestVenueIds[1]} $(echo $RESPONSE | jq .venueId)
+echo "should be Venue 1"
+echo $RESPONSE | jq ".name"
+echo "should be 100"
+echo $RESPONSE | jq ".capacity"
+
+# Verify that an update of an earlier posted venue works - put at api-gateway has no response body
+echo -e "\nTest 20: Verify that an update of an earlier posted venue works"
+body=\
+'{
+  "location": {
+              "streetAddress":"123 Maple St",
+              "city":"CityA",
+              "province":"ProvinceA",
+              "country":"CountryA",
+              "postalCode":"A1A 1A1"
+          },
+          "name":"Venue 1 UPDATED",
+          "capacity":100,
+          "availableDates": [
+              "2024-10-01",
+              "2024-10-02"
+          ]
+}'
+
+assertCurl 200 "curl -X PUT http://$HOST:$PORT/api/v1/venues/${allTestVenueIds[1]} -H \"Content-Type: application/json\" -d '${body}' -s"
+assertEqual ${allTestVenueIds[1]} $(echo $RESPONSE | jq .venueId)
+echo "should be Venue 1 UPDATED"
+echo $RESPONSE | jq ".name"
+echo "should be 100"
+echo $RESPONSE | jq ".capacity"
+
+# Verify that a delete of an earlier posted venue works
+echo -e "\nTest 21: Verify that a delete of an earlier posted venue works"
+assertCurl 204 "curl -X DELETE http://$HOST:$PORT/api/v1/venues/${allTestVenueIds[1]} -s"
+
+# Verify that a 404 (Not Found) status is returned for a non existing venueId (c3540a89-cb47-4c96-888e-ff96708db4d7)
+echo -e "\nTest 22: Verify that a 404 (Not Found) error is returned for a get venue request with a non existing venueId"
+assertCurl 404 "curl http://$HOST:$PORT/api/v1/venues/c3540a89-cb47-4c96-888e-ff96708db4d7 -s"
+
 
 if [[ $@ == *"stop"* ]]
 then
